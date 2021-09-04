02c6d8d7dd38ebddd8a4d9358eadf509539ebf5a
Timo Clasen <goebeltimo@gmail.com>
2021-09-04 14:29:35 +0200

    Add Splitbee Analytics

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ export function Layout({ children, title, description }) {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <script async data-no-cookie data-api="/_hive" src="/bee.js" />
       </Head>
       <main>{children}</main>
     </>
