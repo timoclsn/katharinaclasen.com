f93d6d839d913db38741ad97f5b6d5c923214d68
Katharina Clasen <katharina@codeatelier.com>
2021-11-29 16:09:36 +0100

    Update Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,18 @@ export default function Home() {
       <div className="padding-main">
         <Navigation />
         <div className="text-boxes">
+        <Vita
+            title="Selfemployed UX Designer"
+            date="Since 2013"
+            link={{
+              url: "https://informationlovers.com",
+              name: "Information Lovers",
+            }}
+            description="Helping businesses create lasting and positive digital experiences."
+          />
           <Vita
-            title="Head of Strategy & UX"
-            date="Since 2020"
+            title="Formerly: Head of Strategy & UX"
+            date="2020 - 2021"
             link={{
               url: "https://codeatelier.com",
               name: "Codeatelier GmbH",
@@ -57,15 +66,6 @@ export default function Home() {
             description="Exploring how to design digital products that address
               environmental challenges."
           />
-          <Vita
-            title="Formerly: Founder"
-            date="2013 - 2020"
-            link={{
-              url: "https://informationlovers.com",
-              name: "Information Lovers",
-            }}
-            description="Helping businesses create digital experiences."
-          />
         </div>
       </div>
 
@@ -95,29 +95,13 @@ export default function Home() {
         <div className="text padding-main-left">
           <h2>Hi, I am Katha!</h2>
           <p>
-            I am a technology enthusiast and creative thinker. I am passionate
-            about our environment and want to bring a life-centered view into
-            every part of my life.
+          I am a technology enthusiast and creative thinker. I am passionate about the world we life in and want to bring a life-centered view into every part of my life.
             <br />
-            Our world is changing so rapidly – in many regards not in a good
-            way. I would really like to be at least a small part of the solution
-            – if there is any. Since I have worked with technology for my whole
-            professional life and have a decade of experience as a problem
-            solver and User Experience expert, I try to find ways to to be a
-            positive impact by combining creative problem solving with
-            technology to find new solutions. For the different parts of my
-            professional life this takes different forms:
+            Our world is changing so rapidly. There are a lot of great developments but sadly, there are also a lot of bad ones. And I would really like to be a part of the changes for the better. Since I have worked with technology for my whole professional life and have a decade of experience as a problem solver and User Experience expert, I try to find ways to to be a positive impact by combining creative problem solving, strategic design and UX design with technology to find new solutions. For the different parts of my professional life this takes and took different forms:
           </p>
-          <h3>Head of Strategy & UX at Codeatelier GmbH</h3>
+          <h3>Head of Strategy & UX at Codeatelier GmbH (until dec 2021)</h3>
           <p>
-            After building my own little UX studio with 2 amazing employees,
-            Tanja and Carine, we joined Codeatelier in early 2020. I was very
-            lucky to be offered a position, in which I am able to build up the
-            UX (research, conept and design) and marketing departments and to
-            form the agencies strategy as part of the companies leadership. For
-            me that meant bringing a life-centered perspective and
-            sustainability into the core of the value system, the mission of the
-            agency and our services.
+          After building my own little UX studio with 2 amazing employees, Tanja and Carine, we joined Codeatelier in early 2020. I was very lucky to be offered a position, in which I was able to build up the UX (research, concept and design) and marketing departments and to form the agencies strategy as part of the companies leadership. For me that meant bringing a life-centered perspective and sustainability into the core of the value system, the mission of the agency and the services. With that in mind I sharpened and defined the overall brand, the processes, values and service offering and made sure that this sharpened perspective was transported through the new website, the content marketing and every touchpoint with our customers.
           </p>
           <h3>
             User Experience Lecturer at Stuttgart Media University and
@@ -126,12 +110,7 @@ export default function Home() {
           <p>
             I have been teaching UX Design at the Stuttgart Media University
             since early 2014 and at the University of Applied Sciences Esslingen
-            since early 2020. There I am teaching the students how to build
-            digital products that create positive experiences. A user-centered
-            approach has always been the basis. During the recent years, I came
-            up with a very simple framework to create digital product solutions
-            that aim to solve environmental problems. I am still in the testing
-            phase with that approach.
+            since early 2020. In the course, the students learn how to build digital products that create lasting positive experiences. A human-centered approach has always been the underlying foundation. They get acquainted with important UX terms, methods and frameworks and get to apply their learnings practically by creating a concept and design for an app themselves. They conduct interviews, build out personas, create storyboards, build and usability test interactive prototypes and create a visual Screendesign of their app. This semester (and maybe the next to come), I chose to base the course on a recent report by the German Environment Agency (Umweltbundesamt – UBA) around increasing sustainable consumption through digital product information. The students get to envision solutions, that help consumers make more environmentally-friendly decisions.
           </p>
           <h3>Board member at Makers League e. V.</h3>
           <p>
