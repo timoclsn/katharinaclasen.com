ea08e89d709a1691d76aec61bbacd853221ef999
Timo Clasen <goebeltimo@gmail.com>
2022-12-25 22:10:09 +0100

    Add Some Footer Styles

--- apps/website/src/components/Footer/Footer.tsx
+++ apps/website/src/components/Footer/Footer.tsx
@@ -1,3 +1,9 @@
+import { Container } from "../../design-system/Container/Container";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-background-dark pt-20 pb-36 text-contrast-primary-light">
+      <Container inset>Footer</Container>
+    </footer>
+  );
 };
