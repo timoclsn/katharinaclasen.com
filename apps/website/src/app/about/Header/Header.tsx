443897f2d030f091e62c6eceb9395987a53cb1b0
Timo Clasen <goebeltimo@gmail.com>
2023-01-02 22:22:11 +0100

    Update About Header

--- apps/website/src/app/about/Header/Header.tsx
+++ apps/website/src/app/about/Header/Header.tsx
@@ -10,35 +10,42 @@ import laptopImg from "./assets/laptop.png";
 
 export const Header = () => {
   return (
-    <section className="bg-background-red-candy">
-      <Container inset className="relative pb-28 pt-40">
-        <Heading level="1" className="mb-52 max-w-4xl">
-          Let&apos;s use digital for good and create{" "}
-          <Tag size="xxl">truly positive</Tag> experiences!
-        </Heading>
-        <Body as="p" priority="secondary" className="mb-8 max-w-md">
-          I believe that we need to start to go beyond short-term desires, think
-          holistically and create value in a greater sense in order to build
-          sustainable businesses and become future-ready.
-        </Body>
-        <Button>
-          <ArrowRight />
-          My philosophy
-        </Button>
-        <Image
-          src={memojiImg}
-          alt="Memoji of Katharina Clasen forming a heart with her hands."
-          className="absolute bottom-0 right-0"
-          sizes="30vw"
-          priority
-        />
-      </Container>
+    <>
+      <section className="h-screen bg-background-red-candy">
+        <Container
+          inset
+          className="relative flex h-screen flex-col justify-between pb-28 pt-40"
+        >
+          <Heading level="1" className="max-w-4xl">
+            Let&apos;s use digital for good and create{" "}
+            <Tag size="xxl">truly positive</Tag> experiences!
+          </Heading>
+          <div>
+            <Body as="p" priority="secondary" className="mb-8 max-w-md">
+              I believe that we need to start to go beyond short-term desires,
+              think holistically and create value in a greater sense in order to
+              build sustainable businesses and become future-ready.
+            </Body>
+            <Button>
+              <ArrowRight />
+              My philosophy
+            </Button>
+          </div>
+          <Image
+            src={memojiImg}
+            alt="Memoji of Katharina Clasen forming a heart with her hands."
+            className="absolute bottom-0 right-0"
+            sizes="30vw"
+            priority
+          />
+        </Container>
+      </section>
       <Image
         src={laptopImg}
         alt="Image of a mostly closed laptop."
         className="h-[320px] w-full object-cover"
         sizes="100vw"
       />
-    </section>
+    </>
   );
 };
