1f0b4d0e4bcbb2e0c5a1695fe9481b1659195fbb
Timo Clasen <goebeltimo@gmail.com>
2023-01-21 22:52:13 +0100

    Update usePrevious Hook

--- apps/website/src/hooks/usePrevious.ts
+++ apps/website/src/hooks/usePrevious.ts
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const usePrevious = <State>(state: State) => {
-  const [tuple, setTuple] = useState([null, state]);
-
-  if (tuple[1] !== state) {
-    setTuple([tuple[1], state]);
-  }
-
-  return tuple[0];
+  const ref = useRef(state);
+  useEffect(() => {
+    ref.current = state;
+  }, [state]);
+  return ref.current;
 };
