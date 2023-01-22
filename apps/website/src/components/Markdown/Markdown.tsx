f6a947bff993b2c3c5ba55cd7a9f8cf8a33f77fc
Timo Clasen <goebeltimo@gmail.com>
2023-01-22 15:55:15 +0100

    Fix Markdown Heading Family

--- apps/website/src/components/Markdown/Markdown.tsx
+++ apps/website/src/components/Markdown/Markdown.tsx
@@ -1,18 +1,21 @@
 import { cva, VariantProps } from "class-variance-authority";
 
-export const markdownVariants = cva("font-sans prose", {
-  variants: {
-    color: {
-      light:
-        "text-contrast-secondary-light prose-headings:text-contrast-primary-light",
-      dark: "text-contrast-secondary-dark prose-headings:text-contrast-primary-dark",
+export const markdownVariants = cva(
+  "font-sans prose-headings:font-serif prose",
+  {
+    variants: {
+      color: {
+        light:
+          "text-contrast-secondary-light prose-headings:text-contrast-primary-light",
+        dark: "text-contrast-secondary-dark prose-headings:text-contrast-primary-dark",
+      },
+      size: {
+        normal: "text-base leading-6",
+        large: "text-2xl leading-8",
+      },
     },
-    size: {
-      normal: "text-base leading-6",
-      large: "text-2xl leading-8",
-    },
-  },
-});
+  }
+);
 
 interface Props extends VariantProps<typeof markdownVariants> {
   children: string;
