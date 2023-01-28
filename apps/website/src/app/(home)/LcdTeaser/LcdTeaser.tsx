133c15d6bdb41c2d46b72f6613397626b55c199d
Timo Clasen <goebeltimo@gmail.com>
2023-01-28 21:24:23 +0100

    Add Link

--- apps/website/src/app/(home)/LcdTeaser/LcdTeaser.tsx
+++ apps/website/src/app/(home)/LcdTeaser/LcdTeaser.tsx
@@ -28,7 +28,7 @@ export const LcdTeaser = () => {
             topic by clicking on the button below. Reach out if you have
             questions or want to collaborate!
           </Body>
-          <Button color="light">
+          <Button color="light" href="/lifecentereddesign">
             <ArrowRight />
             More about Life-centered Design
           </Button>
