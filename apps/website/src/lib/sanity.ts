f3b94a19fab07da2df3adf94e5344d637770a685
Timo Clasen <goebeltimo@gmail.com>
2023-02-24 22:02:08 +0100

    Cache Sanity Client Fetch

--- apps/website/src/lib/sanity.ts
+++ apps/website/src/lib/sanity.ts
@@ -1,18 +1,21 @@
 import { createClient } from "next-sanity";
+import { cache } from "react";
 import "server-only";
 import { z } from "zod";
 
-const sanityClient = createClient({
+const client = createClient({
   projectId: "dk9hv6ix",
   dataset: "production",
   apiVersion: "2022-12-21",
   useCdn: false,
 });
 
+const clientFetch = cache(client.fetch.bind(client));
+
 export const queryContent = async <TSchema extends z.ZodTypeAny>(
   query: string,
   schema: TSchema
 ) => {
-  const result = await sanityClient.fetch(query);
+  const result = await clientFetch(query);
   return schema.parse(result) as z.infer<TSchema>;
 };
