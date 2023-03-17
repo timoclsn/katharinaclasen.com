ab64b9eb83be7746c49ab7ab44216e1d22549761
Timo Clasen <goebeltimo@gmail.com>
2023-03-17 20:55:26 +0100

    Fix Number Heading Semantics

--- apps/website/src/components/NumberHeading/NumberHeading.tsx
+++ apps/website/src/components/NumberHeading/NumberHeading.tsx
@@ -10,7 +10,9 @@ export const NumberHeading = ({ children, number }: Props) => {
   return (
     <div className="not-prose mb-6 flex flex-col items-center gap-4 sm:flex-row sm:items-end sm:gap-8">
       <div className="flex h-[80px] w-[80px]  flex-none items-center justify-center rounded-full bg-background-stone sm:h-[120px] sm:w-[120px]">
-        <Heading level="2">{number}</Heading>
+        <Heading as="span" level="2">
+          {number}
+        </Heading>
       </div>
       <Heading level="2">{children}</Heading>
     </div>
