cf2e5b16cf59803277d80f3f88f1b04de5bcb295
Timo Clasen <goebeltimo@gmail.com>
2023-03-18 22:14:49 +0100

    Fix PullQuote Responsivenes

--- apps/website/src/components/PullQuote/PullQuote.tsx
+++ apps/website/src/components/PullQuote/PullQuote.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const PullQuote = ({ children }: Props) => {
   return (
-    <div className="not-prose float-left mr-8 mb-8 max-w-[50%] xl:-ml-32">
+    <div className="not-prose mr-8 mb-8 sm:float-left sm:max-w-[50%] xl:-ml-32">
       <div className="mb-4 h-8 w-1/2 bg-background-secondary" />
       <p className="font-serif text-2xl font-semibold text-contrast-primary-dark sm:text-3xl">
         {children}
