635b33ac23d9fd3c00f9f08c4f2d01b651f48ce5
Timo Clasen <goebeltimo@gmail.com>
2023-03-18 21:07:42 +0100

    Add Icons to Content Box

--- apps/website/src/components/ContentBox/ContentBox.tsx
+++ apps/website/src/components/ContentBox/ContentBox.tsx
@@ -1,14 +1,30 @@
+import {
+  BookOpen,
+  FileText,
+  GraduationCap,
+  Laptop,
+  Newspaper,
+} from "lucide-react";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { Card } from "../../design-system/Card/Card";
 import { Tag } from "../../design-system/Tag/Tag";
 import { Markdown } from "../Markdown/Markdown";
 
+const iconMap = {
+  laptop: Laptop,
+  document: FileText,
+  book: BookOpen,
+  course: GraduationCap,
+  article: Newspaper,
+} as const;
+
 interface Props {
   children: ReactNode;
   links?: Array<{
     title: string;
     url: string;
+    icon?: keyof typeof iconMap;
   }>;
 }
 
@@ -18,17 +34,24 @@ export const ContentBox = ({ children, links }: Props) => {
       <Markdown size="large">{children}</Markdown>
       {links && (
         <div className="mt-8 flex flex-wrap items-center gap-2">
-          {links.map((link, index) => (
-            <Link
-              key={index}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-100 ease-in hover:scale-105 active:scale-95"
-            >
-              <Tag>{link.title}</Tag>
-            </Link>
-          ))}
+          {links.map((link, index) => {
+            const { title, url, icon } = link;
+            const Icon = icon && iconMap[icon];
+            return (
+              <Link
+                key={index}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-transform duration-100 ease-in hover:scale-105 active:scale-95"
+              >
+                <Tag>
+                  {Icon && <Icon />}
+                  {title}
+                </Tag>
+              </Link>
+            );
+          })}
         </div>
       )}
     </Card>
