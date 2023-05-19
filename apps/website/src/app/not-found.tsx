e5c3f94e8c4f65878a2880022cb61baeb57573e8
Timo Clasen <goebeltimo@gmail.com>
2023-05-19 15:06:40 +0200

    Add Simple 404 Page

---
+++ apps/website/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Section } from "../components/Section/Section";
+import { Heading } from "../design-system/Heading/Heading";
+
+const NotFound = () => {
+  return (
+    <Section>
+      <Heading>404 – Page not found</Heading>
+    </Section>
+  );
+};
+
+export default NotFound;
